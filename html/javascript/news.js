f58d75f2f6be49cb944595b5374ace0c1523bf6e
MDonev <donev90@gmail.com>
2020-05-05 17:21:52 +0300

    Initial commit

---
+++ html/javascript/news.js
@@ -0,0 +1,22 @@
+const key = "e6ef2cde327f46e3820d0344025b79fc"
+const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`
+
+const recievedNews = (newsdata) => {
+    const articlesDiv = document.querySelector(".articles")
+    newsdata.articles.forEach((article) => {
+
+        //Here we create and add html elements to our html file
+        const div = document.createElement("div")
+        div.className = "news"
+        div.innerHTML = `
+			<h2>${article.title}</h2>
+			<a href="${article.url}">
+			    <img src="${article.urlToImage}"/>
+			</a>`
+        articlesDiv.appendChild(div)
+    })
+}
+
+fetch(url)
+    .then(response => response.json())
+    .then(recievedNews)
